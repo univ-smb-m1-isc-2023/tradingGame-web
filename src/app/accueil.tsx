6f4c3b813ddb0ee0f3d958d150b451b72ba65da8
TOOm <97885350+TomVoituret@users.noreply.github.com>
2024-03-28 13:39:28 +0100

    tout ok

--- src/app/accueil.tsx
+++ src/app/accueil.tsx
@@ -7,12 +7,14 @@ function Accueil() {
       <div className="absolute inset-0 h-full w-full bg-gray-400/60" />
       <div className="grid min-h-screen px-12">
         <div className="container relative z-10 my-auto mx-auto grid place-items-center text-center">
-        <Typography
+          <Typography
             placeholder=""
             variant="h1"
             color="gray"
-            className="md:max-w-full lg:max-w-8xl font-extrabold text-5xl leading-tight" 
-            onPointerEnterCapture="" onPointerLeaveCapture=""        >
+            className="md:max-w-full lg:max-w-8xl font-extrabold text-5xl leading-tight"
+            onPointerEnterCapture=""
+            onPointerLeaveCapture=""
+          >
             Be the Master of Trading
           </Typography>
 
@@ -20,18 +22,26 @@ function Accueil() {
             placeholder=""
             variant="lead"
             color="white"
-            className="mt-6 mb-10 w-full md:max-w-full lg:max-w-5xl text-lg leading-relaxed"  onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}          >
-            Elevate your trading skills and conquer the global stage. Challenge friends and competitors worldwide. Outwit opponents, navigate diverse markets, and ascend the leaderboard. Can you secure your place as the best in the world?
+            className="mt-6 mb-10 w-full md:max-w-full lg:max-w-5xl text-lg leading-relaxed"
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            Elevate your trading skills and conquer the global stage. Challenge
+            friends and competitors worldwide. Outwit opponents, navigate
+            diverse markets, and ascend the leaderboard. Can you secure your
+            place as the best in the world?
           </Typography>
           <div>
             <Button
               placeholder=""
               variant="gradient"
               color="white"
-              className="py-2 px-6 rounded-md text-lg font-semibold"  onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}            >
+              className="py-2 px-6 rounded-md text-lg font-semibold"
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
               PLAY FREE
             </Button>
-
           </div>
         </div>
       </div>
